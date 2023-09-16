06b8d98890db0e8dc8b687f578d26001b9204c26
Mohamed Ahmed <144061917+MohamedEslam307@users.noreply.github.com>
2023-09-17 00:42:45 +0300

    Create DeepClone.js
    This Script makes independent deep clone for all datatypes in js .

---
+++ DeepClone.js
@@ -0,0 +1,46 @@
+function DeepClone(obj)  // this fumction makes a deepclone from all datatypes in js 
+{ if (typeof obj !="object" && Array.isArray(obj) == false) // if it's not an object and not array 
+    {   
+        return obj ;
+    }
+
+    if (Array.isArray(obj)) // check for being array 
+    {
+        const Cloned_Arr = [] ;
+        for (var i = 0 ; i<obj.length ; i++)
+        {
+            Cloned_Arr[i]=obj[i] ;
+        }
+        return Cloned_Arr ;
+    }
+  // if the Object is from type object 
+  var Cloned_Obj = {} ;
+  for (var key in obj)
+  { if (obj.hasOwnProperty(key))
+    {
+        Cloned_Obj[key] = DeepClone(obj[key]) ;
+    }
+  }
+  return Cloned_Obj ;
+
+
+}
+
+/*
+// for test
+const originalObject = {
+    name: 'Mohamed',
+    age: 22,
+    address: {
+    City: 'Alexandria',
+    Country: 'Egypt',
+    },
+  };
+  
+  const clonedObject = DeepClone(originalObject);
+  
+  console.log(originalObject !== clonedObject); // they are different object // have different addresses (as i expect)
+  console.log(clonedObject) ;
+  clonedObject["name"] = "AboKhalil" ;
+  console.log(originalObject) ;
+  */
